e458a0d0fc4f449959907ac341cf213d35bbb91b
LuisRaico <janampa153@gmail.com>
2021-10-12 17:18:09 -0500

    creacion de mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Simulacro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
